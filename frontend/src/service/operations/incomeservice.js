f296ee91d4efdefbd9f19be91ae00ec3f88f23d1
Iamkaran00 <161184262+Iamkaran00@users.noreply.github.com>
2025-06-23 14:58:02 +0530

    Update incomeservice.js

--- frontend/src/service/operations/incomeservice.js
+++ frontend/src/service/operations/incomeservice.js
@@ -1,24 +1,24 @@
 import api from "../api";
 export const getAllIncome = async ()=>{
-    const response = await api.get('incomes/get_all_incomes');
+    const response = await api.get('/incomes/get_all_incomes');
     return response.data;
 }
 export const createIncome = async(incomedata)=>{
     console.log(incomedata,'incomedata');
     try {
-         const response = await api.post('incomes/add_income',incomedata);
+         const response = await api.post('/incomes/add_income',incomedata);
         return response.data;
     } catch (error) {
         console.log("an error occured",error);
     }
 }
 export const getRecentIncomes = async()=>{
-    const response = await api.get('incomes/getRecentIncomes')
+    const response = await api.get('/incomes/getRecentIncomes')
         return response.data;
     }
 export const deleteIncomes = async(id)=>
 {
  
-const response =     await api.delete(`incomes/deleteIncome/${id}`);
+const response =     await api.delete(`/incomes/deleteIncome/${id}`);
 return response.data;
 }
