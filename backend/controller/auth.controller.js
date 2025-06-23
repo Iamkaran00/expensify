abdeeb31d688c2a629fd9e2aa25027f7c2060602
Iamkaran00 <161184262+Iamkaran00@users.noreply.github.com>
2025-06-23 18:05:36 +0530

    Update auth.controller.js

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -89,6 +89,8 @@ exports.signin = asyncHandler(async (req, res, next) => {
   const options = {
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
     httpOnly: true,
+    sameSite :'None',
+    secure : true,
   };
   res.cookie('token', token, options).status(200).json({
     success: true,
