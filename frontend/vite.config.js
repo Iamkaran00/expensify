228c60015c772dad252322e0f65eedbbfab4d763
Karan Sahu <iamironman892465@gmail.com>
2025-06-23 12:24:26 +0530

    he

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+ 
 export default defineConfig({
-plugins: [react()],
-  base: './',
-})
+  server: {
+    proxy: {
+      '/api/v1': {
+        target: 'https://expensify-6tgi.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
