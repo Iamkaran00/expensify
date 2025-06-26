c0d4f9c3e794988e14fcaa8e9f6077830a649a5c
Iamkaran00 <161184262+Iamkaran00@users.noreply.github.com>
2025-06-26 19:07:40 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
  
 app.use(cors({
-  origin: 'https://expensify-1-eks8.onrender.com', 
+  origin: 'https://expensify-1-eks8.onrender.com/', 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
