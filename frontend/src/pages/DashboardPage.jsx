c8c1247db0a3feea9cfc8618d9074eacc6dffb7e
Karan Sahu <161184262+Iamkaran00@users.noreply.github.com>
2025-08-14 22:23:17 +0530

    Update DashboardPage.jsx

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -207,7 +207,7 @@ const DashboardPage = () => {
         <div className="parent-money-div">
           <div className="boxes">
             <div className={"money-div"}>
-              <p>Remaining Balance</p>
+              <p>Net Balance</p>
               <p>${animatedBalance}</p>
             </div>
 
